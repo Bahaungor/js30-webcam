8e102f4186947f27d80e0df249f598d0db9d46e0
Bahaungor <96170851+Bahaungor@users.noreply.github.com>
2022-02-04 19:29:59 +0300

    Able to load the camera

---
+++ scripts-TRY.js
@@ -0,0 +1,33 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snapSound = document.querySelector('.snap');
+
+function getVideo(){
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            video.srcObject = new MediaStream(localMediaStream);
+            video.play();
+        })
+        .catch(err => console.error(`OH NOOO`, err));
+}
+
+function paintToCanvas(){
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16)
+}
+
+function takePhoto(){
+    
+}
+
+getVideo();
+
--- scripts.js
+++ scripts.js
@@ -3,10 +3,3 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snapSound = document.querySelector('.snap');
-
-function getVideo(){
-    navigator.mediaDevices.getUserMedia({video: true, audio: false})
-        .then(localMediaStream => console.log(localMediaStream))
-}
-
-getVideo()
