b9957078adbf5145892d2a9b8a1bd5e125186723
Bahadir Ungor <bahadirungor@gmail.com>
2022-02-05 17:50:20 +0300

    some modification to js

--- scripts-TRY.js
+++ scripts-TRY.js
@@ -5,11 +5,18 @@ const strip = document.querySelector('.strip');
 const snapSound = document.querySelector('.snap');
 
 function getVideo(){
-    navigator.mediaDevices.getUserMedia({video:true, audio: false})
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
         .then(localMediaStream => {
             video.srcObject = localMediaStream;
             video.play();
         })
+        .catch(err => console.log("Cannot get video due to: ",err));
 }
+
+function paintToCanvas(){
+    [video.width, video.height] = [canvas.width, canvas.height];
+    ctx.drawImage(video);
+}
+
 
 getVideo();
